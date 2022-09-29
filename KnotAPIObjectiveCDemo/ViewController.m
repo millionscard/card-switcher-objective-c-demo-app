d6c094c73b84ca3e1d191e299de44fa4a9d290b3
Nikunjp1904 <nikunjp1904@gmail.com>
2022-09-29 18:14:22 +0530

    Implement register user and create session API.

--- KnotAPIObjectiveCDemo/ViewController.m
+++ KnotAPIObjectiveCDemo/ViewController.m
@@ -13,25 +13,91 @@
 
 @implementation ViewController
 
+bool isFromSwicher;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
 }
 
-- (IBAction)openCardSwitcher:(id)sender {
-    CardOnFileSwitcherSession * session = [[CardOnFileSwitcherSession alloc] initWithSessionId:@"9c361eed-f86c-4db7-8adb-92caca93f8d9" clientId:@"ab86955e-22f4-49c3-97d7-369973f4cb9e" environment:EnvironmentSandbox];
-    [session setPrimaryColorWithPrimaryColor:@"#000000"];
-    [session setTextColorWithTextColor:@"#FFFFFF"];
-    [session setCompanyNameWithCompanyName:@"Millions"];
-    [session openOnCardFileSwitcherWithMerchants:@[]];
-    [session setDelegateWithDelegate:self];
+- (IBAction)onClick:(UIButton *)sender {
+    if (sender.tag == 1) {
+        isFromSwicher = true;
+    }
+    else {
+        isFromSwicher = false;
+    }
+    [self apiCallRegister];
 }
-- (IBAction)openSubscriptionCanceller:(id)sender {
-    CardOnFileSwitcherSession * session = [[CardOnFileSwitcherSession alloc] initWithSessionId:@"9c361eed-f86c-4db7-8adb-92caca93f8d9" clientId:@"ab86955e-22f4-49c3-97d7-369973f4cb9e" environment:EnvironmentSandbox];
-    [session setPrimaryColorWithPrimaryColor:@"#000000"];
-    [session setTextColorWithTextColor:@"#FFFFFF"];
-    [session setCompanyNameWithCompanyName:@"Millions"];
-    [session openOnSubscriptionCancelerWithMerchants:@[]];
-    [session setDelegateWithDelegate:self];
+
+-(void)apiCallRegister{
+    NSString *urlString = @"https://sample.knotapi.com/api/register";
+    NSDictionary *dict = @{ @"first_name" : @"Nikunj", @"last_name" : @"Patel", @"email" : @"production@knotapi.com", @"phone_number" : @"+18024687679", @"password" : @"password", @"address1" : @"348 W 57th St", @"address2" : @"#367", @"state" : @"NY", @"city" : @"NewYork", @"postal_code" : @"10019"};
+    NSString *tokenString = @"";
+    [self apiCall:urlString param:dict token:tokenString];
+}
+
+-(void)apiCallCreateSession:(NSString *)token{
+    NSString *urlString = @"https://sample.knotapi.com/api/knot/session";
+    NSDictionary *dict = [[NSDictionary alloc] init];
+    [self apiCall:urlString param:dict token:token];
+}
+
+
+- (void)apiCall:(NSString *)Url param:(NSDictionary *)parameters token:(NSString *)token{
+    NSMutableURLRequest *urlRequest = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:Url]];
+    
+    NSData* data1 = [NSJSONSerialization dataWithJSONObject:parameters options:NSJSONWritingPrettyPrinted error: nil];
+    
+    [urlRequest setHTTPMethod:@"POST"];
+    [urlRequest setHTTPBody:data1];
+    [urlRequest addValue:@"sandbox" forHTTPHeaderField:@"Environment"];
+    [urlRequest addValue:@"ab86955e-22f4-49c3-97d7-369973f4cb9e" forHTTPHeaderField:@"Client-Id"];
+    [urlRequest addValue:@"d1a5cde831464cd3840ccf762f63ceb7" forHTTPHeaderField:@"Client-Secret"];
+    if([token  isEqual: @""]){
+        [urlRequest addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    }
+    else {
+        [urlRequest addValue:[NSString stringWithFormat:@"Bearer %@",token] forHTTPHeaderField:@"Authorization"];
+    }
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:urlRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+        if(httpResponse.statusCode == 200)
+        {
+            NSError *parseError = nil;
+            NSDictionary *responseDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:&parseError];
+            NSLog(@"The response is - %@",responseDictionary);
+            if([token  isEqual: @""]){
+                NSString *resToken = [responseDictionary objectForKey:@"token"];
+                [self apiCallCreateSession:resToken];
+            }
+            else{
+                NSString *sessionID = [responseDictionary objectForKey:@"session"];
+                if (isFromSwicher == true) {
+                    CardOnFileSwitcherSession * session = [[CardOnFileSwitcherSession alloc] initWithSessionId:sessionID clientId:@"ab86955e-22f4-49c3-97d7-369973f4cb9e" environment:EnvironmentSandbox];
+                    [session setPrimaryColorWithPrimaryColor:@"#000000"];
+                    [session setTextColorWithTextColor:@"#FFFFFF"];
+                    [session setCompanyNameWithCompanyName:@"Millions"];
+                    [session openOnCardFileSwitcherWithMerchants:@[]];
+                    [session setDelegateWithDelegate:self];
+                }
+                else {
+                    CardOnFileSwitcherSession * session = [[CardOnFileSwitcherSession alloc] initWithSessionId:sessionID clientId:@"ab86955e-22f4-49c3-97d7-369973f4cb9e" environment:EnvironmentSandbox];
+                    [session setPrimaryColorWithPrimaryColor:@"#000000"];
+                    [session setTextColorWithTextColor:@"#FFFFFF"];
+                    [session setCompanyNameWithCompanyName:@"Millions"];
+                    [session openOnSubscriptionCancelerWithMerchants:@[]];
+                    [session setDelegateWithDelegate:self];
+                }
+            }
+        }
+        else
+        {
+            NSLog(@"Error");
+        }
+    }];
+    [dataTask resume];
 }
 
 - (void)onSuccessWithMerchant:(NSString *)merchant{
