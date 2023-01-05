7bae865740b4f42cd01ac966f0c494d4beadca45
Nikunjp1904 <40294521+Nikunjp1904@users.noreply.github.com>
2023-01-05 17:14:05 +0530

    Implement register user and create session API. (#2)
    * Implement subscription canceller and update lib version.
    
    * Implement register user and create session API.
    
    * Update sdk version
    Update code
    
    * Update pod to 0.1.45

--- KnotAPIObjectiveCDemo/ViewController.m
+++ KnotAPIObjectiveCDemo/ViewController.m
@@ -13,35 +13,126 @@
 
 @implementation ViewController
 
+bool isFromSwicher;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    CardOnFileSwitcherSession * session = [[CardOnFileSwitcherSession alloc] initWithSessionId:@"8785d82b-c0e5-4eff-b1a9-568ea8a13742"];
-    [session setPrimaryColorWithPrimaryColor:@"#000000"];
-    [session setTextColorWithTextColor:@"#FFFFFF"];
-    [session setCompanyNameWithCompanyName:@"Found"];
-    [session openOnCardFileSwitcherWithMerchants:@[]];
-    [session setDelegateWithDelegate:self];
 }
 
-- (void)onSuccessWithMerchant:(NSString *)merchant{
-    NSLog(@"onSuccessWithMerchant %@", merchant);
+- (IBAction)onClick:(UIButton *)sender {
+    if (sender.tag == 1) {
+        isFromSwicher = true;
+    }
+    else {
+        isFromSwicher = false;
+    }
+    [self apiCallRegister];
 }
 
-- (void)onEventWithEvent:(NSString *)event message:(NSString *)message{
-    NSLog(@"onEventWithEvent %@ %@", event, message);
+-(void)apiCallRegister{
+    NSString *urlString = @"https://sample.knotapi.com/api/register";
+    NSDictionary *dict = @{ @"first_name" : @"Nikunj", @"last_name" : @"Patel", @"email" : @"production@knotapi.com", @"phone_number" : @"+18024687679", @"password" : @"password", @"address1" : @"348 W 57th St", @"address2" : @"#367", @"state" : @"NY", @"city" : @"NewYork", @"postal_code" : @"10019"};
+    NSString *tokenString = @"";
+    [self apiCall:urlString param:dict token:tokenString];
 }
 
-- (void)onErrorWithError:(NSString *)error message:(NSString *)message{
-    NSLog(@"onErrorWithError %@ %@", error, message);
+-(void)apiCallCreateSession:(NSString *)token{
+    NSString *urlString = @"https://sample.knotapi.com/api/knot/session";
+    NSDictionary *dict = [[NSDictionary alloc] init];
+    [self apiCall:urlString param:dict token:token];
 }
 
-- (void)onExit{
-    NSLog(@"onExit");
+
+- (void)apiCall:(NSString *)Url param:(NSDictionary *)parameters token:(NSString *)token{
+    NSMutableURLRequest *urlRequest = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:Url]];
+    
+    NSData* data1 = [NSJSONSerialization dataWithJSONObject:parameters options:NSJSONWritingPrettyPrinted error: nil];
+    
+    [urlRequest setHTTPMethod:@"POST"];
+    [urlRequest setHTTPBody:data1];
+    if([token  isEqual: @""]){
+        [urlRequest addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    }
+    else {
+        [urlRequest addValue:[NSString stringWithFormat:@"Bearer %@",token] forHTTPHeaderField:@"Authorization"];
+    }
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:urlRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+        if(httpResponse.statusCode == 200)
+        {
+            NSError *parseError = nil;
+            NSDictionary *responseDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:&parseError];
+            NSLog(@"The response is - %@",responseDictionary);
+            if([token  isEqual: @""]){
+                NSString *resToken = [responseDictionary objectForKey:@"token"];
+                [self apiCallCreateSession:resToken];
+            }
+            else{
+                NSString *sessionID = [responseDictionary objectForKey:@"session"];
+                if (isFromSwicher == true) {
+                    CardOnFileSwitcherSession * session = [[CardOnFileSwitcherSession alloc] initWithSessionId:sessionID clientId:@"3f4acb6b-a8c9-47bc-820c-b0eaf24ee771" environment:EnvironmentSandbox];
+                    [session setPrimaryColorWithPrimaryColor:@"#000000"];
+                    [session setTextColorWithTextColor:@"#FFFFFF"];
+                    [session setCompanyNameWithCompanyName:@"Rho"];
+                    
+                    CardSwitcherConfiguration *cardSwitcherConfig = [[CardSwitcherConfiguration alloc] init];
+                    [cardSwitcherConfig setOnSuccessOnSuccess:^(NSString *merchant) {
+                        NSLog(@"CardSwitcher Merchant:- %@",merchant);
+                    }];
+                    [cardSwitcherConfig setOnExitOnExit:^{
+                        NSLog(@"CardSwitcher onExit");
+                    }];
+                    [cardSwitcherConfig setOnErrorOnError:^(NSString * error, NSString * message) {
+                        NSLog(@"CardSwitcher Error:- %@ Message: %@",error, message);
+                    }];
+                    [cardSwitcherConfig setOnEventOnEvent:^(NSString * event, NSString * message) {
+                        NSLog(@"CardSwitcher Event:- %@ Merchant: %@",error, message);
+                    }];
+                    [cardSwitcherConfig setOnFinishedOnFinished:^{
+                        NSLog(@"CardSwitcher Finished");
+                    }];
+                    [session setConfigurationWithConfig:cardSwitcherConfig];
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        [session openCardOnFileSwitcher];
+                    });
+                }
+                else {
+                    SubscriptionCancelerSession * session = [[SubscriptionCancelerSession alloc] initWithSessionId:sessionID clientId:@"3f4acb6b-a8c9-47bc-820c-b0eaf24ee771" environment:EnvironmentSandbox];
+                    [session setPrimaryColorWithPrimaryColor:@"#000000"];
+                    [session setTextColorWithTextColor:@"#FFFFFF"];
+                    [session setCompanyNameWithCompanyName:@"Millions"];
+                    [session setAmountWithAmount:true];
+                    SubscriptionCancelerConfiguration *config = [[SubscriptionCancelerConfiguration alloc] init];
+                    [config setOnSuccessOnSuccess:^(NSString *merchant) {
+                        NSLog(@"SubscriptionCanceler onSuccess Merchant:- %@",merchant);
+                    }];
+                    [config setOnExitOnExit:^{
+                        NSLog(@"SubscriptionCanceler onExit");
+                    }];
+                    [config setOnErrorOnError:^(NSString * error, NSString * message) {
+                        NSLog(@"SubscriptionCanceler Error:- %@ Message: %@",error, message);
+                    }];
+                    [config setOnEventOnEvent:^(NSString * event, NSString * message) {
+                        NSLog(@"SubscriptionCanceler Event:- %@ Merchant: %@",error, message);
+                    }];
+                    [config setOnFinishedOnFinished:^{
+                        NSLog(@"SubscriptionCanceler Finished");
+                    }];
+                    [session setConfigurationWithConfiguration: config];
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        [session openSubscriptionCanceler];
+                    });
+                }
+            }
+        }
+        else
+        {
+            NSLog(@"Error");
+        }
+    }];
+    [dataTask resume];
 }
 
-- (void)onFinished{
-    NSLog(@"onFinished");
-
-}
 @end
